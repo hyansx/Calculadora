af049c46e18520635403548feb76ccdabcd932c9
Hyan <hyancrvg@gmail.com>
2023-12-12 17:02:39 -0300

    first commit

---
+++ a.py
@@ -0,0 +1,8 @@
+Times = input(('Você é Vasco ou Botafogo?: '))
+
+if Times == 'Vasco':
+    print('VOCÊ É VASCO NUNCA VÃO ENTENDER ESSE AMOR')
+elif Times == 'Botafogo':
+    print('VOCÊ FOI ESOLHIDO')
+else:
+    print('Você não torce pelos Gigantes do Rio')
